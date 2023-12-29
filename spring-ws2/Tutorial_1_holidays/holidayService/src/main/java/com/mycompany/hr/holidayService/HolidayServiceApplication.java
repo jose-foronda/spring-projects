589d41ecd0dc1db871e343638927dbed0c64c5cb
jose-foronda <jose09621@hotmail.com>
2023-12-29 14:57:07 -0500

    spring projects

---
+++ spring-ws2/Tutorial_1_holidays/holidayService/src/main/java/com/mycompany/hr/holidayService/HolidayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.hr.holidayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayServiceApplication.class, args);
+	}
+
+}
---
+++ spring-ws2/Tutorial_1_holidays/holidayService/src/main/java/com/mycompany/hr/holidayService/hr/service/HumanResourceService.java
@@ -0,0 +1,10 @@
+package com.mycompany.hr.holidayService.hr.service;
+
+
+import java.util.Date;
+
+public interface HumanResourceService {
+    void bookHoliday(Date startDate, Date endDate, String name);
+}
+
+
